422489e0232e56d91befd23885527c5b136dccbe
Evan Longwood <evanmlongwood@gmail.com>
2018-05-27 15:40:39 -0700

    Combining method.

--- part1_ch2.py
+++ part1_ch2.py
@@ -6,7 +6,13 @@ print(message)
 message = "Now I need to go get something to eat!\n"
 print(message)
 
-name = "ada lovelace"
+name = "ada lovelace'\n"
 print(name.title())
 print(name.upper())
 print(name.lower())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print(full_name.title())
