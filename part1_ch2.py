74f3175ffd76007675d705efecabaef73fd73f0b
Evan Longwood <evanmlongwood@gmail.com>
2018-05-27 15:23:49 -0700

    Updated text. More stuff added from the lesson.

--- part1_ch2.py
+++ part1_ch2.py
@@ -1,4 +1,7 @@
 # This is more or less a review on Variables
 
-message = "Hello World! I'm pretty hungry for a burrito."
+message = "Hello World! I'm pretty hungry for a burrito.\n"
+print(message)
+
+message = "Now I need to go get something to eat!"
 print(message)
