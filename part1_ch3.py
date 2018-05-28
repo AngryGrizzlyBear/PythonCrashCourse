88460372771d798b884eccee660374c281f4f406
Evan Longwood <evanmlongwood@gmail.com>
2018-05-28 12:20:46 -0700

    Calling lists in a variable.

--- part1_ch3.py
+++ part1_ch3.py
@@ -2,3 +2,10 @@
 bicycles = ['trek', 'cannodale', 'redline', 'specialized']
 print(bicycles)
 print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[1].title())
+print(bicycles[3].title())
+print(bicycles[-1].title())
+
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
