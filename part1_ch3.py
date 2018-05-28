396ec42bad5cb1633777efa5c3cf066f06b8d0aa
Evan Longwood <evanmlongwood@gmail.com>
2018-05-28 16:23:45 -0700

    del function

--- part1_ch3.py
+++ part1_ch3.py
@@ -43,3 +43,13 @@ motorcycles = ['honda', 'yamaha', 'suzuki']
 motorcycles.insert(0, 'ducati')
 print(motorcycles)
 
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+del motorcycles[0]
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+del motorcycles[1]
+print(motorcycles)
