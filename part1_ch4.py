98a7f3d4d8bbe1387113a07460a3a51fc7fbcf82
Evan Longwood <evanmlongwood@gmail.com>
2018-06-01 21:06:18 -0700

    Adding more features to list.

--- part1_ch4.py
+++ part1_ch4.py
@@ -3,3 +3,9 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician.title())
+
+# Doing more with a for loop
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print("\n" + magician.title() + ", you did a fantastic trick!")
