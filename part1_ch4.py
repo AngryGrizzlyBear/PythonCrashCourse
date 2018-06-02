8b407ca48f9e67613dce4bf04ab49f27fd6eb34e
Evan Longwood <evanmlongwood@gmail.com>
2018-06-01 21:21:52 -0700

    Adding even MORE features to the foor loop

--- part1_ch4.py
+++ part1_ch4.py
@@ -9,3 +9,10 @@ for magician in magicians:
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print("\n" + magician.title() + ", you did a fantastic trick!")
+
+# Doing even more with a for loop
+
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print("\n" + magician.title() + ", you did a fantastic trick!")
+    print("Can't wait to see your next trick, " + magician.title() + "!\n")
