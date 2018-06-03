1284053662f0df2faeb39bc152ce0ace7d51e0a8
Evan Longwood <evanmlongwood@gmail.com>
2018-06-02 22:00:07 -0700

    List Comprehension

--- part1_ch4.py
+++ part1_ch4.py
@@ -67,3 +67,7 @@ digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 print(min(digits))
 print(max(digits))
 print(sum(digits))
+
+# List Comprehensions
+square = [value**2 for value in range(1,11)]
+print(squares)
