b8a13e2c8c853d486188c994d9c3dfc0eaf08d45
Evan <evanmlongwood@gmail.com>
2018-06-05 11:39:18 -0700

    Looping through the tuple values

--- part1_ch4.py
+++ part1_ch4.py
@@ -193,3 +193,9 @@ for pizza in friend_pizza:
 dimensions = (200, 50)
 print(dimensions[0])
 print(dimensions[1])
+print("\n")
+
+# Looping through the values
+dimensions = (200, 50)
+for dimension in dimensions:
+    print(dimension)
