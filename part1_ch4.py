4d051bc123cfd5d032e4b26d936750bf873e3f11
Evan Longwood <evanmlongwood@gmail.com>
2018-06-04 22:37:16 -0700

    Proof that these items are copying over.

--- part1_ch4.py
+++ part1_ch4.py
@@ -147,4 +147,16 @@ print(my_foods)
 print("\nMy Friend's faborite foods are:")
 print(friend_foods)
 
+# adding to the list
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("my favorite foods are:")
+print(my_foods)
+
+print("\nMy Friend's faborite foods are:")
+print(friend_foods)
 
