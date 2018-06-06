e6326987c88635bf08744a75af70483518f359d8
Evan Longwood <evanmlongwood@gmail.com>
2018-06-05 23:19:15 -0700

    Numbers

--- part1_ch5.py
+++ part1_ch5.py
@@ -14,3 +14,10 @@ requested_topping = 'mushrooms'
 
 if requested_topping != 'anchovies':
     print("Hold the anchovies!")
+
+# Numbers
+
+answer = 17
+
+if answer != 42:
+    print("That is not the correct answer. Please try again!")
