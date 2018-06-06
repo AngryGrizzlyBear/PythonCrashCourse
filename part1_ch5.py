437aa024037d5cdcdf0011129a93ba3a785b5938
Evan <evanmlongwood@gmail.com>
2018-06-06 10:58:42 -0700

    Charging old people

--- part1_ch5.py
+++ part1_ch5.py
@@ -88,12 +88,14 @@ elif age < 18:
 else:
     print("Your admission costs $10.")
 
-age = 12
+age = 65
 
 if age < 4:
     price = 0
 elif age < 18:
     price = 5
+elif age < 65:
+    price = 10
 else:
     price = 10
 
