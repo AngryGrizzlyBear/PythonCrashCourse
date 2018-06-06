e1d548a9b833f1089387e1918568f7fa113dc967
Evan <evanmlongwood@gmail.com>
2018-06-06 08:37:23 -0700

    Checking whether a value is not in a list

--- part1_ch5.py
+++ part1_ch5.py
@@ -21,3 +21,10 @@ answer = 17
 
 if answer != 42:
     print("That is not the correct answer. Please try again!")
+
+# Checking whether a value is not in a list
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+    print(user.title() + ", you can post a response if you wish.")
