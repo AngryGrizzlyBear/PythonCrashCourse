23e7eb8ee5feae177bd56a28beaf0a2c2f428b9d
Evan Longwood <evanmlongwood@gmail.com>
2018-06-06 22:02:59 -0700

    Error statement

--- part1_ch5.py
+++ part1_ch5.py
@@ -171,6 +171,10 @@ if 'strawberries' in favorite_fruit:
 requested_toppings = ['mushroms', 'green peppers', 'extra cheese']
 
 for requested_topping in requested_toppings:
-    print("Adding " + requested_topping + ".")
+    if requested_topping == 'green peppers':
+        print("Sorry, we're out of green peppers right now.")
+    else:
+        print( "Adding " + requested_topping + ".")
 
 print("finished making your pizza!")
+
