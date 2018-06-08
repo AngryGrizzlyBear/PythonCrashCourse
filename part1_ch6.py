10abdd3759910ead021e1ebd16c5af715e4299d8
Evan Longwood <evanmlongwood@gmail.com>
2018-06-07 21:41:12 -0700

    Printing Alien in a different manner.

--- part1_ch6.py
+++ part1_ch6.py
@@ -16,3 +16,11 @@ print(alien_0)
 alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 print(alien_0)
+
+# Starting with an Empty Dictionary
+alien_0 = {}
+
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
