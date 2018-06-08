eb767c91f0b48edf42ffd2f0d58e08c5b0cbaef7
Evan Longwood <evanmlongwood@gmail.com>
2018-06-07 22:15:37 -0700

    Deleting features.

--- part1_ch6.py
+++ part1_ch6.py
@@ -44,9 +44,16 @@ elif alien_0['speed'] == 'medium':
     x_increment = 2
 else:
     # This must be a fast alien.
-    x_increment == 3
+    x_increment = 3
 
 # The new position is the old position plus the increment.
 alien_0['x_position'] = alien_0['x_position'] + x_increment
 
 print("New x-position: " + str(alien_0['x_position']))
+
+# Removing Key Value Pair
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+del alien_0['points']
+print(alien_0)
