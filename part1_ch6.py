89bce9e97f7ec47f5bd53fb15c4a467a05188184
Evan Longwood <evanmlongwood@gmail.com>
2018-06-07 21:38:34 -0700

    Positioning the alien

--- part1_ch6.py
+++ part1_ch6.py
@@ -7,3 +7,12 @@ print(alien_0['points'])
 # Working with Dictionaries
 new_points = alien_0['points']
 print("You just earned " + str(new_points) + " points!")
+
+# Adding new key-value pairs
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
