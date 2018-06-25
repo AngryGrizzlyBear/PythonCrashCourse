5735270822376c20cd2e29e5a95fa99df1a503ab
Evan <evanmlongwood@gmail.com>
2018-06-25 14:06:28 -0700

    Aliens revisted

--- part1_ch6.py
+++ part1_ch6.py
@@ -236,3 +236,36 @@ for coder in coders:
         print("Thank you for taking the poll, " + coder.title() + "!")
     else:
         print(coder.title() + ", what's your favorite programming language?")
+
+print("\n")
+# Nesting
+
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+
+aliens = [alien_0, alien_1, alien_2]
+
+for alien in aliens:
+    print(alien)
+
+aliens = []
+
+# make 30 green aliens
+
+for alien in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[0:3]:
+    if alien['color'] == 'green':
+        alien['color'] == 'yellow'
+        alien['speed'] == 'medium'
+        alien['points'] = 10
+# Show the first 5 aliens
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+
+# Show how many aliens have been created.
+print("Total number of aliens: " + str(len(aliens)))
