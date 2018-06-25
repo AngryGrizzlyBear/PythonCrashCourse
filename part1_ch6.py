f8ca15c81f4c1e4bad054f416282a48d10e2b520
Evan <evanmlongwood@gmail.com>
2018-06-25 13:41:24 -0700

    6-6 polls.

--- part1_ch6.py
+++ part1_ch6.py
@@ -215,3 +215,24 @@ for river in rivers.keys():
 print("\nThe following countries are included in this data set:")
 for country in rivers.values():
     print("- " + country.title())
+
+# 6-6
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for name, language in favorite_languages.items():
+    print(name.title() + "'s favorite language is " + language.title() + ".")
+
+print("\n")
+
+coders = ['phil', 'josh', 'david', 'becca', 'sarah', 'matt', 'danielle']
+for coder in coders:
+    if coder in favorite_languages.keys():
+        print("Thank you for taking the poll, " + coder.title() + "!")
+    else:
+        print(coder.title() + ", what's your favorite programming language?")
