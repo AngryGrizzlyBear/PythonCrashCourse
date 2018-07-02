92092bb03ece635c142ef96e02edc5226b81f35a
Evan <evanmlongwood@gmail.com>
2018-07-02 11:37:30 -0700

    6-9 exercise

--- part1_ch6.py
+++ part1_ch6.py
@@ -367,3 +367,16 @@ for pet in pets:
     print("\nHere's what I know about " + pet['name'].title() + ":")
     for key, value in pet.items():
         print("\t" + key + ': ' + str(value))
+
+# 6- 9
+favorite_places = {
+    'eric': ['bear mountain', 'death valley', 'tierra del fuego'],
+    'erin': ['hawaii', 'iceland'],
+    'ever': ['mt. verstovia', 'the playground', 'south carolina']
+}
+
+for name, places in favorite_places.items():
+    print("\n" + name.title() + " likes the following places:")
+    for place in places:
+        print("- " + place.title())
+
