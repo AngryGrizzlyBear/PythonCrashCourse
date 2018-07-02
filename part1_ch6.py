d3dd8c61b2179aa87b0731e87ed42e59e1ff49f6
Evan <evanmlongwood@gmail.com>
2018-07-02 13:36:24 -0700

    6-11 Cities

--- part1_ch6.py
+++ part1_ch6.py
@@ -392,3 +392,32 @@ for name, numbers in favorite_numbers.items():
     print("\n" + name.title() + " likes the following numbers:")
     for number in numbers:
         print(" " + str(number))
+
+# 6-11 Cities
+cities = {
+    'santiago': {
+        'country': 'chile',
+        'population': 6158080,
+        'nearby mountains': 'andes',
+    },
+    'talkeetna': {
+        'country': 'alaska',
+        'population': 876,
+        'nearby mountains': 'alaska range',
+    },
+    'kathmandu': {
+        'country': 'nepal',
+        'population': 1003285,
+        'nearby mountains': 'himilaya',
+    }
+
+}
+
+for city, city_info in cities.items():
+    country = city_info['country'].title()
+    population = city_info['population']
+    mountains = city_info['nearby mountains'].title()
+
+    print("\n" + city.title() + " is in " + country + ".")
+    print("  It has a population of about " + str(population) + ".")
+    print("  The " + mountains + " mountainsa are nearby.")
