d6b267fa15674de621ae40848ebff6cec04cf7cd
Evan <evanmlongwood@gmail.com>
2018-07-03 09:06:20 -0700

    Greeting someone, using input.

---
+++ part1_ch7.py
@@ -0,0 +1,6 @@
+# How the input function works
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+
+name = input("Please enter your name: ")
+print("Hello, " + name + "!")
