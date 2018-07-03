0c5c307b48924b3571e682696bc1edd44f634466
Evan <evanmlongwood@gmail.com>
2018-07-03 09:36:20 -0700

    Using int again for user input testing.

--- part1_ch7.py
+++ part1_ch7.py
@@ -4,3 +4,23 @@ print(message)
 
 name = input("Please enter your name: ")
 print("Hello, " + name + "!")
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+prompt += "\nWhat is your first name? "
+
+name = input(prompt)
+print("\nHello, " + name + "!")
+
+# Using int to Accept Numerical Input
+age = input("How old are you? ")
+print("\n You are " + age + " years old!")
+age = int(age)
+print(age >= 18)
+
+# rollercoaster
+height = input("How tall are you in inches? ")
+height = int(height)
+if height >= 36:
+    print("\nYou're tall enough to ride!")
+else:
+    print("\nYou'll be able to ride when you're a little older.")
