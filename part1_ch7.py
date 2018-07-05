902bf528d1f5ca6b062f75465b43198a114d6c38
Evan <evanmlongwood@gmail.com>
2018-07-05 11:28:54 -0700

    More loop practice.

--- part1_ch7.py
+++ part1_ch7.py
@@ -66,6 +66,10 @@ while current_number <= 5:
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt = "\nEnter 'quit' to end this program. "
 message = ""
-while message != 'quit':
+active = True
+while active:
     message = input(prompt)
-    print(message)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
